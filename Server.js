f5e0e223e76fa0a4df7069d9f5eb1d10afd00374
jeshu-collab <jeshu241125@gmail.com>
2026-09-15 19:55:02 +0530

    starting express js

--- Server.js
+++ Server.js
@@ -1,10 +1,20 @@
 const http = require("http");
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
-    console.log("New req");
-    res.end("Hello World");
+const app = express();
+app.get('/', (req, res) => {
+    return res.send("Hello World");
 });
+
+app.get('/about', (req, res) => {
+    return res.send(`Hello, ${req.query.k} you are ${req.query.age} years old`);
+});
+
+app.listen(8000, () => console.log("Server is running on port 8000"));
+
+
+/*const myServer = http.createServer(app);
 
 myServer.listen(8000, () => {
     console.log("Server is running on port 8000");
-})
+})*/
